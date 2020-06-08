cef284d23faccbdd000fc5e81e478cf58ec943c9
bio1600project <fzhao8@gmail.com>
2020-06-07 20:28:53 -0700

    followed guide

---
+++ server/build/precache-manifest.5480752e6e41486d300601363f18be7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f774d2c88f1e46c58dc7c5d36026f3f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a354056084233c09170",
+    "url": "/static/css/2.a1251ad2.chunk.css"
+  },
+  {
+    "revision": "9bd4a9cee85191fdd4c3",
+    "url": "/static/css/main.774a0b30.chunk.css"
+  },
+  {
+    "revision": "9a354056084233c09170",
+    "url": "/static/js/2.f532abd1.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.f532abd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd4a9cee85191fdd4c3",
+    "url": "/static/js/main.3da4a182.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
